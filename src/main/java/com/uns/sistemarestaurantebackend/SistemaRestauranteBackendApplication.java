c6d56bf3c6e855f8150cbe381c0720cca12d7eff
Lucas Solmoni <solmonilucas@gmail.com>
2026-05-01 22:53:11 -0300

    Configuración inicial del backend y conexión a Neon

---
+++ src/main/java/com/uns/sistemarestaurantebackend/SistemaRestauranteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uns.sistemarestaurantebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestauranteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaRestauranteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uns/sistemarestaurantebackend/SistemaRestauranteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uns.sistemarestaurantebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRestauranteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
