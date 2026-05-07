11f6d63a5f1d843ad4f2c4d9b9d545ebfec39fbe
Gonzalo Perez <zalogonpe@hotmail.com>
2026-05-07 15:53:40 -0300

    Definición de estados

---
+++ frontend/app/lib/definitions.ts
@@ -0,0 +1,28 @@
+// app/lib/definitions.ts
+export enum Roles {
+  Administrador = "Administrador",
+  Cocinero = "Cocinero",
+  Mozo = "Mozo"
+};
+
+export enum EstadoMesa {
+  Libre = "libre",
+  Ocupada = "ocupada"
+};
+
+export enum EstadoComanda {
+  Pendiente = "pendiente",
+  Prepracion = "en preparacion",
+  Lista = "lista",
+  Entregada = "entregada",
+  Cerrada = "cerrada",
+  Cancelada = "cancelada"
+};
+
+export enum EstadoItem {
+  Pendiente = "pendiente",
+  Preparacion = "en preparacion",
+  Listo = "listo",
+  Entregado = "entregado",
+  Cancelado = "cancelado"
+}
