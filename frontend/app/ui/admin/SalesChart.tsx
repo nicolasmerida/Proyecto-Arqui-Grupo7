699f8027f00e170a75d20dfc591afe0ae567dd1b
Gonzalo Perez <zalogonpe@hotmail.com>
2026-06-18 13:23:28 -0300

    Corrección tipo Comanda
    Se corrigió el import del tipo de Comanda, según lo que se necesita, y se acomodo la estructura de la página de pagos

--- frontend/app/ui/admin/SalesChart.tsx
+++
@@ -1,9 +0,0 @@
-// app/ui/admin/SalesChart.tsx
-export default function SalesChart() {
-
-    return (
-        <div className="flex flex-col">
-            Gráfico de ventas
-        </div>
-    );
-}
